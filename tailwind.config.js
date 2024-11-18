a313876d3d1245ffab98401123cf119021a3bb07
mamitiana <mamitianarandriamihaja2806@gmaol.com>
2024-11-18 15:32:57 +0300

    mamitiana

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+  },
+    extend: {
+    },
+  
+  plugins: [],
+  colors: {
+  },
+  tropical: {
+  },
+  light:'#FFF9DB',
+  DEFAULT:'#FDE68A',
+  dark:'#FBBF24',
+};
+
